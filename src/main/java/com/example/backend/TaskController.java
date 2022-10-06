036d592dab85c349bbf92c49a7b301c827537db7
Jan Dalecki <jan.dalecki@klosebrothers.de>
2022-10-06 15:40:58 +0200

    Added very simple logging with log4j2

--- src/main/java/com/example/backend/TaskController.java
+++ src/main/java/com/example/backend/TaskController.java
@@ -1,5 +1,7 @@
 package com.example.backend;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,35 +12,43 @@ import java.util.List;
 @RequestMapping("/todo")
 public class TaskController {
     private final TaskService service;
+    private final Logger log = LogManager.getLogger();
     @Autowired
     public TaskController(TaskService service){
+        log.info("Controller created");
         this.service = service;
     }
     @PostMapping("")
     public Task createTask(@RequestBody Task newTask){
+        log.info("Create task");
         return service.saveTask(newTask);
     }
 
     @GetMapping("/{id}")
     public Task getTaskById(@PathVariable Long id) {
+        log.info("GET task");
         return service.getTaskById(id);
     }
 
     @GetMapping("")
     public List<Task> getAllTasks() {
+        log.info("GET all tasks");
         return service.getAllTasks();
     }
 
     @PutMapping("/{id}/completion/true")
     public Task completeTask(@PathVariable Long id) {
+        log.info("Completed task");
         return service.completeTask(id, true);
     }
     @PutMapping("/{id}/completion/false")
     public Task uncompleteTask(@PathVariable Long id) {
+        log.info("Uncompleted task");
         return service.completeTask(id, false);
     }
     @DeleteMapping("/{id}")
     public void deleteTask(@PathVariable Long id){
+        log.info("Deleted task");
         service.deleteTask(id);
     }
 }
--- src/test/java/com/example/backend/TodoAppApplicationTests.java
+++ src/test/java/com/example/backend/TodoAppApplicationTests.java
@@ -13,7 +13,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import org.intellij.lang.annotations.Language;
-import org.springframework.test.web.servlet.ResultActions;
 
 import java.io.UnsupportedEncodingException;
 
